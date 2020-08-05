4d3677e0bee70001737ca0dbe1dbac994dff67a9
Yusuf <yusufyilmazfr@gmail.com>
2020-08-05 22:32:43 +0300

    Başvuru durumları için enum oluşturuldu.

---
+++ Devnot.Mentor.Api/Enums/MentorMenteePairStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevnotMentor.Api.Enums
+{
+    public enum MentorMenteePairStatus
+    {
+        Waiting,
+        Approved,
+        Rejected
+    }
+
+    public static class MentorMenteePairStatusExtensions
+    {
+        public static int? ToInt(this MentorMenteePairStatus status)
+        {
+            return (int?)status;
+        }
+    }
+
+}
