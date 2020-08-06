08df5a6e7ab92b19d7e7ffbf2d8ab190b88f5ebb
Yusuf <yusufyilmazfr@gmail.com>
2020-08-06 18:32:03 +0300

    Jwt token servis implementasyonu yapıldı.

---
+++ Devnot.Mentor.Api/Utilities/Security/Token/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DevnotMentor.Api.Utilities.Security.Token
+{
+    public interface ITokenService
+    {
+        TokenInfo CreateToken(int userId, string userName);
+        IEnumerable<Claim> ReadToken(string token);
+    }
+}
