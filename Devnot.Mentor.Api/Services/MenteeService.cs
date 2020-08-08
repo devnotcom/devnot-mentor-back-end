20f0dcd183f0834c1b6bd7bb241867f5f8a5e4e7
Yusuf <yusufyilmazfr@gmail.com>
2020-08-08 16:32:44 +0300

    Aspect oriented programming implementasyonu yapıldı, BaseService silindi.

--- Devnot.Mentor.Api/Services/MenteeService.cs
+++ Devnot.Mentor.Api/Services/MenteeService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using DevnotMentor.Api.Aspects.Autofac.Exception;
+using DevnotMentor.Api.Aspects.Autofac.UnitOfWork;
 using DevnotMentor.Api.Common;
 using DevnotMentor.Api.Controllers;
 using DevnotMentor.Api.Entities;
@@ -17,7 +19,7 @@ using System.Transactions;
 
 namespace DevnotMentor.Api.Services
 {
-    public class MenteeService : BaseService, IMenteeService
+    public class MenteeService : IMenteeService
     {
         MenteeRepository menteeRepository;
         MenteeLinksRepository menteeLinksRepository;
@@ -27,7 +29,10 @@ namespace DevnotMentor.Api.Services
         MentorRepository mentorRepository;
         MentorApplicationsRepository mentorApplicationsRepository;
 
-        public MenteeService(IOptions<AppSettings> appSettings, IOptions<ResponseMessages> responseMessages, IMapper mapper, MentorDBContext context) : base(appSettings, responseMessages, mapper, context)
+        ResponseMessages responseMessages;
+        IMapper mapper;
+
+        public MenteeService(IOptions<ResponseMessages> responseMessages, IMapper mapper, MentorDBContext context)
         {
             // TODO: I will take fields with dependency injection when application successfully done :)
 
@@ -38,19 +43,55 @@ namespace DevnotMentor.Api.Services
             userRepository = new UserRepository(context);
             mentorRepository = new MentorRepository(context);
             mentorApplicationsRepository = new MentorApplicationsRepository(context);
+
+            this.responseMessages = responseMessages.Value;
+            this.mapper = mapper;
         }
 
-        public async Task<ApiResponse<MenteeProfileModel>> GetMenteeProfile(string userName)
+        [ExceptionHandlingAspect]
+        public ApiResponse<MenteeProfileModel> GetMenteeProfile(string userName)
         {
             var response = new ApiResponse<MenteeProfileModel>();
 
-            await RunInTry(response, async () =>
-            {
-                var user = userRepository.Filter(u => u.UserName == userName).FirstOrDefault();
+            var user = userRepository.Filter(u => u.UserName == userName).FirstOrDefault();
 
-                if (user != null)
+            if (user != null)
+            {
+                var mentee = menteeRepository.Filter(m => m.UserId == user.Id).FirstOrDefault();
+
+                if (mentee != null)
                 {
-                    var mentee = menteeRepository.Filter(m => m.UserId == user.Id).FirstOrDefault();
+                    response.Data = mapper.Map<MenteeProfileModel>(mentee);
+                    response.Success = true;
+                }
+                else
+                {
+                    response.Message = responseMessages.Values["MenteeNotFound"];
+                }
+            }
+            else
+            {
+                response.Message = responseMessages.Values["UserNotFound"];
+            }
+
+            return response;
+        }
+
+        [DevnotUnitOfWorkAspect]
+        [ExceptionHandlingAspect]
+        public async Task<ApiResponse<MenteeProfileModel>> CreateMenteeProfile(MenteeProfileModel model)
+        {
+            var response = new ApiResponse<MenteeProfileModel>();
+
+            var user = userRepository.Filter(u => u.UserName == model.UserName).FirstOrDefault();
+
+            if (user != null)
+            {
+                var isRegisteredMentee = menteeRepository.Filter(m => m.UserId == user.Id).Any();
+
+                if (!isRegisteredMentee)
+                {
+                    var mentee = await CreateNewMentee(model, user);
 
                     if (mentee != null)
                     {
@@ -59,106 +100,59 @@ namespace DevnotMentor.Api.Services
                     }
                     else
                     {
-                        response.Message = responseMessages.Values["MenteeNotFound"];
+                        response.Message = responseMessages.Values["UnhandledException"];
                     }
                 }
                 else
                 {
-                    response.Message = responseMessages.Values["UserNotFound"];
+                    response.Message = responseMessages.Values["AlreadyRegisteredMentee"];
                 }
-            });
-
-            return response;
-        }
-
-        public async Task<ApiResponse<MenteeProfileModel>> CreateMenteeProfile(MenteeProfileModel model)
-        {
-            var response = new ApiResponse<MenteeProfileModel>();
-
-            await RunInTry(response, async () =>
+            }
+            else
             {
-
-                var user = userRepository.Filter(u => u.UserName == model.UserName).FirstOrDefault();
-
-                if (user != null)
-                {
-                    var isRegisteredMentee = menteeRepository.Filter(m => m.UserId == user.Id).Any();
-
-                    if (!isRegisteredMentee)
-                    {
-                        var mentee = await CreateNewMentee(model, user);
-
-                        if (mentee != null)
-                        {
-                            response.Data = mapper.Map<MenteeProfileModel>(mentee);
-                            response.Success = true;
-                        }
-                        else
-                        {
-                            response.Message = responseMessages.Values["UnhandledException"];
-                        }
-                    }
-                    else
-                    {
-                        response.Message = responseMessages.Values["AlreadyRegisteredMentee"];
-                    }
-                }
-                else
-                {
-                    response.Message = responseMessages.Values["UserNotFound"];
-                }
-
-            });
+                response.Message = responseMessages.Values["UserNotFound"];
+            }
 
             return response;
         }
 
-
-
-
-        //public void UpdateMenteeProfile(MenteeProfileModel model)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
+        [ExceptionHandlingAspect]
+        [DevnotUnitOfWorkAspect]
         private async Task<Mentee> CreateNewMentee(MenteeProfileModel model, User user)
         {
             Mentee mentee = null;
 
-            using (var transaction = new TransactionScope())
+            var newMentee = mapper.Map<Mentee>(model);
+            newMentee.UserId = user.Id;
+
+            mentee = menteeRepository.Create(newMentee);
+            if (mentee != null)
             {
-                var newMentee = mapper.Map<Mentee>(model);
-                newMentee.UserId = user.Id;
+                menteeLinksRepository.Create(mentee.Id, model.MenteeLinks);
 
-                mentee = menteeRepository.Create(newMentee);
-                if (mentee != null)
+                foreach (var menteeTag in model.MenteeTags)
                 {
-                    menteeLinksRepository.Create(mentee.Id, model.MenteeLinks);
-
-                    foreach (var menteeTag in model.MenteeTags)
+                    var tag = tagRepository.Filter(t => t.Name == menteeTag).FirstOrDefault();
+                    if (tag != null)
                     {
-                        var tag = tagRepository.Filter(t => t.Name == menteeTag).FirstOrDefault();
-                        if (tag != null)
+                        menteeTagsRepository.Create(new MenteeTags { TagId = tag.Id, MenteeId = mentee.Id });
+                    }
+                    else
+                    {
+                        var newTag = tagRepository.Create(new Tag { Name = menteeTag });
+                        if (newTag != null)
                         {
-                            menteeTagsRepository.Create(new MenteeTags { TagId = tag.Id, MenteeId = mentee.Id });
-                        }
-                        else
-                        {
-                            var newTag = tagRepository.Create(new Tag { Name = menteeTag });
-                            if (newTag != null)
-                            {
-                                menteeTagsRepository.Create(new MenteeTags { TagId = newTag.Id, MenteeId = mentee.Id });
-                            }
+                            menteeTagsRepository.Create(new MenteeTags { TagId = newTag.Id, MenteeId = mentee.Id });
                         }
                     }
-                    await menteeRepository.SaveChangesAsync();
-                    transaction.Complete();
                 }
+                await menteeRepository.SaveChangesAsync();
             }
 
             return mentee;
         }
 
+        [ExceptionHandlingAspect]
         public async Task<ApiResponse> ApplyToMentor(ApplyMentorModel model)
         {
             var apiResponse = new ApiResponse();
--- Devnot.Mentor.Api/Services/UserService.cs
+++ Devnot.Mentor.Api/Services/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using DevnotMentor.Api.Aspects.Autofac.Exception;
+using DevnotMentor.Api.Aspects.Autofac.UnitOfWork;
 using DevnotMentor.Api.Common;
 using DevnotMentor.Api.Entities;
 using DevnotMentor.Api.Helpers;
@@ -22,66 +24,72 @@ namespace DevnotMentor.Api.Services
 {
     //TODO: Aynı username ile kayıt yapılabiliyor
 
-    public class UserService : BaseService, IUserService
+    public class UserService : IUserService
     {
-        UserRepository repository;
+        private UserRepository repository;
         private ITokenService tokenService;
 
-        public UserService(IOptions<AppSettings> appSettings, IOptions<ResponseMessages> responseMessages, IMapper mapper, MentorDBContext context, ITokenService tokenService) : base(appSettings, responseMessages, mapper, context)
+        ResponseMessages responseMessages;
+        AppSettings appSettings;
+        IMapper mapper;
+
+        public UserService(IOptions<AppSettings> appSettings, IOptions<ResponseMessages> responseMessages, IMapper mapper, MentorDBContext context, ITokenService tokenService)
         {
             repository = new UserRepository(context);
+
             this.tokenService = tokenService;
+            this.responseMessages = responseMessages.Value;
+            this.mapper = mapper;
+            this.appSettings = appSettings.Value;
         }
 
+        [ExceptionHandlingAspect]
         public async Task<ApiResponse<User>> Login(LoginModel model)
         {
             var response = new ApiResponse<User>();
 
-            await RunInTry(response, async () =>
+            var user = await repository.GetUser(model.UserName, model.Password);
+
+            if (user == null)
             {
-                var user = await repository.GetUser(model.UserName, model.Password);
+                response.Message = responseMessages.Values["InvalidUserNameOrPassword"];
+            }
+            else if (!user.UserNameConfirmed.HasValue || !user.UserNameConfirmed.Value)
+            {
+                response.Message = responseMessages.Values["UserNameNotValidated"];
+            }
+            else
+            {
+                var tokenData = tokenService.CreateToken(user.Id, user.UserName);
 
-                if (user == null)
-                {
-                    response.Message = responseMessages.Values["InvalidUserNameOrPassword"];
-                }
-                else if (!user.UserNameConfirmed.HasValue || !user.UserNameConfirmed.Value)
-                {
-                    response.Message = responseMessages.Values["UserNameNotValidated"];
-                }
-                else
-                {
-                    var tokenData = tokenService.CreateToken(user.Id, user.UserName);
+                user.Token = tokenData.Token;
+                user.TokenExpireDate = tokenData.ExpiredDate;
 
-                    user.Token = tokenData.Token;
-                    user.TokenExpireDate = tokenData.ExpiredDate;
-
-                    response = new ApiResponse<User> { Success = true, Data = user };
-                }
-            });
+                response.Success = true;
+                response.Data = user;
+            }
 
             return response;
         }
 
+        [ExceptionHandlingAspect]
+        [DevnotUnitOfWorkAspect]
         public async Task<ApiResponse<User>> Register(UserModel model)
         {
             var response = new ApiResponse<User>();
 
-            await RunInTry(response, async () =>
+            if (!FileHelper.IsValidProfileImage(model.ProfileImage))
             {
-                if (!FileHelper.IsValidProfileImage(model.ProfileImage))
-                {
-                    response.Message = responseMessages.Values["InvalidProfileImage"];
-                    return;
-                }
+                response.Message = responseMessages.Values["InvalidProfileImage"];
+                return response;
+            }
 
-                model.ProfileImageUrl = await FileHelper.UploadProfileImage(model.ProfileImage, appSettings);
-                var newUser = repository.Create(mapper.Map<User>(model));
-                await repository.SaveChangesAsync();
+            model.ProfileImageUrl = await FileHelper.UploadProfileImage(model.ProfileImage, appSettings);
+            var newUser = repository.Create(mapper.Map<User>(model));
+            await repository.SaveChangesAsync();
 
-                response.Data = newUser;
-                response.Success = true;
-            });
+            response.Data = newUser;
+            response.Success = true;
 
             return response;
         }
