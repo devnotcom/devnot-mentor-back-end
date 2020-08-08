7e8c648900323cd8fe847ebbfd96634fcbee8021
Yusuf <yusufyilmazfr@gmail.com>
2020-08-08 12:46:23 +0300

    refactoring: transaction scope eklendi ve default return value oluşturma işlemi ExceptionHandlingAspect içerisine taşındı.

--- Devnot.Mentor.Api/Aspects/Autofac/UnitOfWork/DevnotUnitOfWorkAspect.cs
+++ Devnot.Mentor.Api/Aspects/Autofac/UnitOfWork/DevnotUnitOfWorkAspect.cs
@@ -1,12 +1,5 @@
 ﻿using Castle.DynamicProxy;
-using DevnotMentor.Api.Common;
 using DevnotMentor.Api.Utilities.Interceptor.Autofac;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Transactions;
 
@@ -16,41 +9,25 @@ namespace DevnotMentor.Api.Aspects.Autofac.UnitOfWork
     {
         public override void Intercept(IInvocation invocation)
         {
-            try
+            using (var transaction = new TransactionScope())
             {
-                invocation.Proceed();
-
-                if (invocation.ReturnValue is Task task && task.Exception != null)
+                try
                 {
-                    if (invocation.Method.ReturnType.IsGenericType)
+                    invocation.Proceed();
+
+                    if (invocation.ReturnValue is Task task && task.Exception != null)
                     {
-                        Type[] argTypes = invocation.Method.ReturnType.GetGenericArguments();
-
-                        var args = new List<object>();
-
-                        foreach (var argType in argTypes)
-                        {
-                            args.Add(Activator.CreateInstance(argType));
-                        }
-
-                        MethodInfo fromResult = typeof(Task).GetMethod("FromResult");
-
-                        MethodInfo genericFromResult = fromResult.MakeGenericMethod(argTypes);
-
-                        invocation.ReturnValue = genericFromResult.Invoke(task, args.ToArray());
+                        throw task.Exception;
                     }
 
-                    throw task.Exception.InnerException;
+                    transaction.Complete();
                 }
-            }
-            catch (Exception ex)
-            {
-                if (invocation.Method.ReturnType.IsAbstract)
+                catch (System.Exception ex)
                 {
-                    throw new InvalidOperationException($"Type {invocation.Method.ReturnType.FullName} may not be abstract. Abstract classes cannot be instantiated.");
-                }
+                    transaction.Dispose();
 
-                throw ex;
+                    throw ex;
+                }
             }
         }
     }
