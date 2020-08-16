7e3df8d0533034cbe1f4cdf6cba39df137b6c2d1
Yusuf <yusufyilmazfr@gmail.com>
2020-08-16 12:04:12 +0300

    User tablosuna Email alanı eklendi.

--- Devnot.Mentor.Api/Entities/User.cs
+++ Devnot.Mentor.Api/Entities/User.cs
@@ -13,6 +13,7 @@ namespace DevnotMentor.Api.Entities
 
         public int Id { get; set; }
         public string UserName { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string SurName { get; set; }
