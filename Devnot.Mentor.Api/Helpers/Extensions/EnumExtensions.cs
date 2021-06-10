46e2a39ad8e98b3e4cff2bb16b87327913b53c83
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-06-10 13:12:37 +0300

    Rename enum extension class name

---
+++ Devnot.Mentor.Api/Helpers/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevnotMentor.Api.Helpers.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static int? ToInt(this Enum status)
+        {
+            return Convert.ToInt32(status);
+        }
+    }
+}
