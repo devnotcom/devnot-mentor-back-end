f78fb2fc82bee5f39d35e0f84367081cff1b4bfc
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-07 23:58:22 +0300

    Add new prop for dto

--- Devnot.Mentor.Api/CustomEntities/Dto/MentorApplicationsDto.cs
+++ Devnot.Mentor.Api/CustomEntities/Dto/MentorApplicationsDto.cs
@@ -4,6 +4,7 @@ namespace DevnotMentor.Api.CustomEntities.Dto
 {
     public class MentorApplicationsDto
     {
+        public int Id { get; set; }
         public DateTime? ApplyDate { get; set; }
         public DateTime? CompleteDate { get; set; }
         public string ApllicationNotes { get; set; }
