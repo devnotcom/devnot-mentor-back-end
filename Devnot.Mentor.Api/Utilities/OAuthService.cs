9404c1bacdeaf999dfa320072d5b5b730aa33225
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-26 03:02:59 +0300

    Add GetResponseContentAsStringAsync to get responses from one point

--- Devnot.Mentor.Api/Utilities/OAuthService.cs
+++ Devnot.Mentor.Api/Utilities/OAuthService.cs
@@ -14,40 +14,43 @@ namespace DevnotMentor.Api.Utilities
 {
     public static class OAuthService
     {
+        private static async Task<string> GetResponseContentAsStringAsync(HttpRequestMessage request, OAuthCreatingTicketContext ctx)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await ctx.Backchannel.SendAsync(request, ctx.HttpContext.RequestAborted);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private static async Task<TOAuthResponse> GetUserPublicInformationsAsync<TOAuthResponse>(OAuthCreatingTicketContext ctx)
+        {
+            var publicInfoRequest = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+            var publicInfoResponse = await GetResponseContentAsStringAsync(publicInfoRequest, ctx);
+
+            return JsonConvert.DeserializeObject<TOAuthResponse>(publicInfoResponse);
+        }
+
+        private static async Task<List<OAuthGitHubEmailResponse>> GetGitHubEmailsAsync(OAuthCreatingTicketContext ctx)
+        {
+            var emailRequest = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/emails");
+            var emailResponse = await GetResponseContentAsStringAsync(emailRequest, ctx);
+
+            return JsonConvert.DeserializeObject<List<OAuthGitHubEmailResponse>>(emailResponse);
+        }
+
         public static async Task<OAuthGitHubUser> GetOAuthGitHubUserAsync(OAuthCreatingTicketContext ctx)
         {
-            var authGitHubResponse = await GetOAuthUserPublicInformationAsync<OAuthGitHubResponse>(ctx);
+            var authGitHubResponse = await GetUserPublicInformationsAsync<OAuthGitHubResponse>(ctx);
             authGitHubResponse.Emails = await GetGitHubEmailsAsync(ctx);
-
+            
             return authGitHubResponse.MapToOAuthGitHubUser();
         }
 
         public static async Task<OAuthGoogleUser> GetOAuthGoogleUserAsync(OAuthCreatingTicketContext ctx)
         {
-            var authGoogleResponse = await GetOAuthUserPublicInformationAsync<OAuthGoogleResponse>(ctx);
+            var authGoogleResponse = await GetUserPublicInformationsAsync<OAuthGoogleResponse>(ctx);
             return authGoogleResponse.MapToOAuthGoogleUser();
-        }
-
-        public static async Task<List<OAuthGitHubEmailResponse>> GetGitHubEmailsAsync(OAuthCreatingTicketContext ctx)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/emails");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var response = await ctx.Backchannel.SendAsync(request, ctx.HttpContext.RequestAborted);
-            response.EnsureSuccessStatusCode();
-
-            return JsonConvert.DeserializeObject<List<OAuthGitHubEmailResponse>>(await response.Content.ReadAsStringAsync());
-        }
-
-        public static async Task<TOAuthResponse> GetOAuthUserPublicInformationAsync<TOAuthResponse>(OAuthCreatingTicketContext ctx)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var response = await ctx.Backchannel.SendAsync(request, ctx.HttpContext.RequestAborted);
-            response.EnsureSuccessStatusCode();
-
-            return JsonConvert.DeserializeObject<TOAuthResponse>(await response.Content.ReadAsStringAsync());
         }
 
         public static async Task SignInAsync(OAuthUser oAuthUser, HttpContext httpContext)
