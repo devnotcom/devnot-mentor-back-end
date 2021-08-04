5c3bea3f6e6a30d7a1cc251c9d90cb91d4c9ec4c
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-04 16:18:20 +0300

    Implement error writing in error handling

--- Devnot.Mentor.Api/Repositories/Interfaces/ILoggerRepository.cs
+++ Devnot.Mentor.Api/Repositories/Interfaces/ILoggerRepository.cs
@@ -1,7 +1,5 @@
 ﻿using DevnotMentor.Api.Entities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevnotMentor.Api.Repositories.Interfaces
