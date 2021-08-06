00985347981e6372b1b1322d9ec8e24aabbf48a5
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-08-06 17:26:13 +0300

    TagDto, MenteeTagDto, MentorTagDto were created

--- Devnot.Mentor.Api/CustomEntities/Dto/MenteeDto.cs
+++ Devnot.Mentor.Api/CustomEntities/Dto/MenteeDto.cs
@@ -1,12 +1,20 @@
-﻿namespace DevnotMentor.Api.CustomEntities.Dto
+﻿using System.Collections.Generic;
+
+namespace DevnotMentor.Api.CustomEntities.Dto
 {
     public class MenteeDto
     {
+        public MenteeDto()
+        {
+            MenteeTags = new List<MenteeTagDto>();
+        }
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int? UserId { get; set; }
 
         public UserDto User { get; set; }
+        public List<MenteeTagDto> MenteeTags { get; set; }
     }
 }
