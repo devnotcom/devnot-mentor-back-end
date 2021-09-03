dd763d127f6610bb2c7c81b4d10602a8392fc299
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-09-04 01:34:52 +0300

    add ServiceCollectionExtensions and update Startup.cs

---
+++ src/DevnotMentor.WebAPI/Helpers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+using DevnotMentor.Business.Services;
+using DevnotMentor.Business.Services.Interfaces;
+using DevnotMentor.Business.Utilities.Email;
+using DevnotMentor.Business.Utilities.Email.SmtpMail;
+using DevnotMentor.Business.Utilities.File;
+using DevnotMentor.Business.Utilities.File.Local;
+using DevnotMentor.Business.Utilities.Security.Hash;
+using DevnotMentor.Business.Utilities.Security.Hash.Sha256;
+using DevnotMentor.Business.Utilities.Security.Token;
+using DevnotMentor.Business.Utilities.Security.Token.Jwt;
+using DevnotMentor.Configurations.Context;
+using DevnotMentor.Configurations.Environment;
+using DevnotMentor.Data;
+using DevnotMentor.Data.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevnotMentor.WebAPI.Helpers.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDataRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ILogRepository, LogRepository>();
+            services.AddScoped<IMenteeLinkRepository, MenteeLinkRepository>();
+            services.AddScoped<IMenteeRepository, MenteeRepository>();
+            services.AddScoped<IMenteeTagRepository, MenteeTagRepository>();
+            services.AddScoped<IApplicationRepository, ApplicationRepository>();
+            services.AddScoped<IMentorLinkRepository, MentorLinkRepository>();
+            services.AddScoped<IMentorshipRepository, MentorshipRepository>();
+            services.AddScoped<IMentorRepository, MentorRepository>();
+            services.AddScoped<IMentorTagRepository, MentorTagRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddBussinessServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IMentorService, MentorService>();
+            services.AddScoped<IMenteeService, MenteeService>();
+            services.AddScoped<IMentorshipService, MentorshipService>();
+            services.AddScoped<IApplicationService, ApplicationService>();
+            return services;
+        }
+
+        public static IServiceCollection AddBusinessUtilities(this IServiceCollection services)
+        {
+            services.AddScoped<IMailService, SmtpMailService>();
+            services.AddScoped<IFileService, LocalFileService>();
+
+            services.AddSingleton<ITokenService, JwtTokenService>();
+            services.AddSingleton<IHashService, Sha256HashService>();
+            services.AddSingleton<IDevnotConfigurationContext, DevnotConfigurationContext>();
+            services.AddSingleton<IEnvironmentService, EnvironmentService>();
+
+            return services;
+        }
+    }
+}
--- src/DevnotMentor.WebAPI/Startup.cs
+++ src/DevnotMentor.WebAPI/Startup.cs
@@ -1,7 +1,4 @@
 using System;
-using DevnotMentor.Data.Entities;
-using DevnotMentor.Business.Services;
-using DevnotMentor.Business.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -9,19 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DevnotMentor.WebAPI.ActionFilters;
-using DevnotMentor.Business.Utilities.Security.Token;
-using DevnotMentor.Configurations.Context;
 using DevnotMentor.Configurations.Environment;
 using DevnotMentor.WebAPI.Middlewares;
-using DevnotMentor.Business.Utilities.Security.Hash;
-using DevnotMentor.Business.Utilities.Security.Hash.Sha256;
-using DevnotMentor.Business.Utilities.Email;
 using DevnotMentor.Data;
-using DevnotMentor.Data.Interfaces;
-using DevnotMentor.Business.Utilities.Email.SmtpMail;
-using DevnotMentor.Business.Utilities.File;
-using DevnotMentor.Business.Utilities.File.Local;
-using DevnotMentor.Business.Utilities.Security.Token.Jwt;
+using DevnotMentor.WebAPI.Helpers.Extensions;
 
 namespace DevnotMentor.WebAPI
 {
@@ -36,58 +24,29 @@ namespace DevnotMentor.WebAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = EnvironmentService.StaticConfiguration["ConnectionStrings:SQLServerConnectionString"];
-            services.AddDbContext<MentorDBContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<MentorDBContext>(options => options.UseSqlServer(
+                EnvironmentService.StaticConfiguration["ConnectionStrings:SQLServerConnectionString"]
+            ));
 
-            services.AddControllers()
-                .AddNewtonsoftJson(options =>
-                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                );
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            services.AddBusinessUtilities();
+            services.AddBussinessServices();
+            services.AddDataRepositories();
+            services.AddCustomSwagger();
 
             services.AddAutoMapper(typeof(Startup));
-
             services.AddSingleton<TokenAuthentication>();
-
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IMentorService, MentorService>();
-            services.AddScoped<IMenteeService, MenteeService>();
-            services.AddScoped<IMentorshipService, MentorshipService>();
-            services.AddScoped<IApplicationService, ApplicationService>();
-
-
-            services.AddScoped<IMailService, SmtpMailService>();
-            services.AddScoped<IFileService, LocalFileService>();
-
-            services.AddSingleton<ITokenService, JwtTokenService>();
-            services.AddSingleton<IHashService, Sha256HashService>();
-            services.AddSingleton<IDevnotConfigurationContext, DevnotConfigurationContext>();
-            services.AddSingleton<IEnvironmentService, EnvironmentService>();
-
-            #region Repositories
-
-            services.AddScoped<ILogRepository, LogRepository>();
-            services.AddScoped<IMenteeLinkRepository, MenteeLinkRepository>();
-            services.AddScoped<IMenteeRepository, MenteeRepository>();
-            services.AddScoped<IMenteeTagRepository, MenteeTagRepository>();
-            services.AddScoped<IApplicationRepository, ApplicationRepository>();
-            services.AddScoped<IMentorLinkRepository, MentorLinkRepository>();
-            services.AddScoped<IMentorshipRepository, MentorshipRepository>();
-            services.AddScoped<IMentorRepository, MentorRepository>();
-            services.AddScoped<IMentorTagRepository, MentorTagRepository>();
-            services.AddScoped<ITagRepository, TagRepository>();
-            services.AddScoped<IUserRepository, UserRepository>();
-
-            #endregion
 
             services.AddCors(options =>
             {
-                options
-                .AddPolicy("AllowMyOrigin", builder =>
+                options.AddPolicy("AllowMyOrigin", builder =>
                     builder
-                    //.WithOrigins("http://localhost:8080")
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
             });
 
             services.AddMvc(options =>
@@ -101,16 +60,10 @@ namespace DevnotMentor.WebAPI
                 options.JsonSerializerOptions.IgnoreNullValues = true;
             });
 
-            services.AddCustomSwagger();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
             app.UseAuthentication();
             app.UseCors("AllowMyOrigin");
